a37e74f0f28c26dd226c85a488f7abb2e0693c42
SoniaMirdha07 <114904640+SoniaMirdha07@users.noreply.github.com>
2022-10-09 21:47:55 +0530

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the  number of row");
+        int n = sc.nextInt();
+
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=n-i;j++){
+               System.out.print("  ");
+           }
+           for(int k=i;k>=1;k--){
+               System.out.print(k+" ");
+           }
+           for (int j=2;j<=i;j++){
+               System.out.print(j+" ");
+           }
+
+           System.out.println();
+
+       }
+    }
+}
+
